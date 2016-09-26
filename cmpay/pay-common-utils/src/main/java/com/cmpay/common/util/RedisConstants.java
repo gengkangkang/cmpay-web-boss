471156aa9e210b56e55281a51263e5a25101e60c
gkk <tjeagle@163.com>
2016-09-26 09:43:07 +0800

    redis工具类

---
+++ cmpay/pay-common-utils/src/main/java/com/cmpay/common/util/RedisConstants.java
@@ -0,0 +1,13 @@
+package com.cmpay.common.util;
+/**
+ * Redis相关常量，统一管理
+ * @author gengkangkang
+ * @E-mail gengkangkang@cm-inv.com
+ *
+ * 2016年9月22日 下午5:23:56
+ *
+ */
+public class RedisConstants {
+
+       public static final String CMPAY_IP_BINDING_LIST="CMPAY_IP_BINDING_LIST";
+}
