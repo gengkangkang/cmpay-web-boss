a023e3c1d8bc0a42ef10899d206ad774c0a4cab7
luoxun <luoxun@cm-inv.com>
2016-10-18 11:14:42 +0800

    第一次

--- cmpay/pay-service-llpay/src/main/java/com/cmpay/service/llpay/dao/TestUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cmpay.service.llpay.dao;
-
-import com.cmpay.service.llpay.model.TestUser;
-
-public interface TestUserMapper {
-    int insert(TestUser record);
-
-    int insertSelective(TestUser record);
-}
