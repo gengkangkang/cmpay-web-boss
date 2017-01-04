6cf9403afa21adb7887d741eb63bd999e0bc1895
gkk <tjeagle@163.com>
2017-01-04 13:22:13 +0800

    up

--- src/main/java/com/cmpay/boss/controller/OrderRecordController.java
+++ src/main/java/com/cmpay/boss/controller/OrderRecordController.java
@@ -1,28 +1,17 @@
 package com.cmpay.boss.controller;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.cmpay.boss.domain.CutOrderBO;
 import com.cmpay.boss.domain.OrderRecordBO;
-import com.cmpay.boss.form.CutOrderForm;
 import com.cmpay.boss.form.OrderRecordForm;
 import com.cmpay.boss.service.OrderRecordService;
 import com.cmpay.boss.util.DateUtil;
-import com.cmpay.boss.util.EncodeUtil;
 import com.cmpay.boss.util.Pagination;
 /**
  * 交易订单管理
@@ -33,14 +22,13 @@ import com.cmpay.boss.util.Pagination;
  */
 
 @Controller
-@RequestMapping(value = "/orderManagement")
 public class OrderRecordController {
     private static final Logger logger = LoggerFactory.getLogger(ConfigController.class);
 
     @Autowired
     OrderRecordService    orderRecordService;
-    
-    @RequestMapping(value = "/query_all_order", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/orderManagement/query_all_order", method = RequestMethod.GET)
     public String getAllOrderRecord(@ModelAttribute("orderRecordForm") OrderRecordForm orderRecordForm){
         OrderRecordBO orderRecordBo = new OrderRecordBO();
         String pageCurrent = orderRecordForm.getPageCurrent();
@@ -54,10 +42,10 @@ public class OrderRecordController {
         orderRecordForm.setPagination(orderRecordBOPagination);
 
         return "order/orderrecordlist";
-       
+
     }
-    
-    @RequestMapping(value = "/getOrderRecordByPara", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/orderManagement/getOrderRecordByPara", method = RequestMethod.POST)
     public String getOrderRecordByPara(@ModelAttribute("orderRecordForm") OrderRecordForm orderRecordForm){
     	OrderRecordBO orderRecordBO = new OrderRecordBO();
         String pageCurrent = orderRecordForm.getPageCurrent();
